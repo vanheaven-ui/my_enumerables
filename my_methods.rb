b060ff05e36607eda271c086f4d29a5e1c27635e
vanheaven <vanheaven6@gmail.com>
2020-05-24 00:33:24 +0300

    Add my_each_with_index method

--- my_methods.rb
+++ my_methods.rb
@@ -16,7 +16,22 @@ module Enumerable
       index += 1
     end
     self
-  end  
+  end
+  
+  def my_each_with_index
+    index = 0
+    return to_enum(:my_each_with_index) unless block_given?
+
+    until index >= self.size
+      if self.kind_of?(Range) or (Hash)
+        yield(self.to_a[index], index)
+      else
+        yield(self[index], index)
+      end
+      index += 1
+    end
+    self
+  end
 end
 
 arr = [1, 2, 3, 4, 5, 6]
@@ -32,5 +47,16 @@ r = (0..6)
 
 # p r.my_each { |e| e }
 # p r.each { |e| e }
+# puts "---------------------------------------"
+
+# p arr.my_each_with_index { |e, i| p "#{i}. #{e}" }
+# p arr.each_with_index { |e, i| p "#{i}. #{e}" }
+
+# p h.my_each_with_index { |e, i| puts "#{i}. #{e}" }
+# p h.each_with_index { |e, i| puts "#{i}. #{e}" }
+
+# p r.my_each_with_index { |e, i| "#{i}. #{e}" }
+# p r.each_with_index { |e, i| "#{i}. #{e}" }
+# puts "---------------------------------------"
 
 # rubocop: disable Style/CaseEquality
