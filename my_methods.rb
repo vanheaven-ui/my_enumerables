02bad5123e0abab10d75038212df66fd55c10dfe
vanheaven <vanheaven6@gmail.com>
2020-05-27 10:58:53 +0300

    Update my_map method

--- my_methods.rb
+++ my_methods.rb
@@ -152,31 +152,18 @@ module Enumerable
     end
   end
 
-  def my_map(prc, &block)
+  def my_map
     return_arr = []
-    if prc.is_a?(Proc) && block_given?
-      if is_a?(Hash)
-        my_each do |key, value|
-          return_arr << prc.call(key, value)
-        end
-      else
-        my_each do |element|
-          return_arr << prc.call(element)
-        end
+    if is_a?(Hash)
+      my_each do |key, value|
+        return_arr << yield(key, value)
       end
-      return_arr
-    elsif prc.nil? && block_given?
-      if is_a?(Hash)
-        my_each do |key, value|
-          return_arr << block.call(key, value)
-        end
-      else
-        my_each do |element|
-          return_arr << block.call(element)
-        end
+    else
+      my_each do |element|
+        return_arr << yield(element)
       end
-      return_arr
     end
+    return_arr
   end
 
   def my_inject(acc = nil, oper = nil)
@@ -219,6 +206,33 @@ module Enumerable
       else
         raise ArgumentError, 'Please provide a symbol'
       end
+    end
+  end
+
+  def my_map_with_proc_and_block(prc, &block)
+    return_arr = []
+    if prc.is_a?(Proc) && block_given?
+      if is_a?(Hash)
+        my_each do |key, value|
+          return_arr << prc.call(key, value)
+        end
+      else
+        my_each do |element|
+          return_arr << prc.call(element)
+        end
+      end
+      return_arr
+    elsif prc.nil? && block_given?
+      if is_a?(Hash)
+        my_each do |key, value|
+          return_arr << block.call(key, value)
+        end
+      else
+        my_each do |element|
+          return_arr << block.call(element)
+        end
+      end
+      return_arr
     end
   end
 end
