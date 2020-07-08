c1a9a80ca5d1f27fa97cbdd38c653ac24d30e508
vanheaven <vanheaven6@gmail.com>
2020-07-08 12:43:49 +0300

    Add rspec test for my_each_with_index and my_select

--- spec/my_methods_spec.rb
+++ spec/my_methods_spec.rb
@@ -21,4 +21,42 @@ describe Enumerable do
       end
     end
   end
+
+  describe '#my_each_with_index' do
+    context 'when block is not given' do
+      it 'return enumerator' do
+        expect(a.my_each_with_index).to be_instance_of(Enumerator)
+      end
+    end
+
+    context 'when block is given' do
+      it 'return self' do
+        expect(a.my_each_with_index {|x| x * 2}).to eq(a)
+        expect(b.my_each_with_index {|x| x * 2}).to eq(b)
+        expect(c.my_each_with_index {|key,value| value * 2}).to eq(c)
+      end
+    end
+  end
+
+  describe '#my_select' do
+    context 'when block is not given' do
+      it 'return enumerator' do
+        expect(a.my_select).to be_instance_of(Enumerator)
+      end
+    end
+
+    context 'when block is given' do
+      it 'return array of array elements that are true in the block' do
+        expect(a.my_select {|element| element > 2 }).to eql([3, 4, 5])
+      end
+
+      it 'return array of hash key value pairs that are true in the block' do
+        expect(c.my_select { |key, value| value == 2 }).to eql({:b=>2})
+      end
+
+      it 'return array of range elements that are true in the block' do
+        expect(b.my_select { |element| element >= 5 }).to eql([5, 6, 7, 8, 9])
+      end
+    end
+  end
 end
